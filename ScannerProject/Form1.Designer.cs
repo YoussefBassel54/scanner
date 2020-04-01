a6ccba4baf49ac2a6bd88c54ae88b7fa6803b583
YoussefBassel54 <53179104+YoussefBassel54@users.noreply.github.com>
2020-04-01 22:03:14 +0200

    Add files via upload

---
+++ ScannerProject/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ScannerProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScanBtn = new System.Windows.Forms.Button();
+            this.InputTxt = new System.Windows.Forms.RichTextBox();
+            this.OutputTxt = new System.Windows.Forms.RichTextBox();
+            this.ClrBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ScanBtn
+            // 
+            this.ScanBtn.Location = new System.Drawing.Point(190, 399);
+            this.ScanBtn.Name = "ScanBtn";
+            this.ScanBtn.Size = new System.Drawing.Size(101, 25);
+            this.ScanBtn.TabIndex = 0;
+            this.ScanBtn.Text = "Scan";
+            this.ScanBtn.UseVisualStyleBackColor = true;
+            this.ScanBtn.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // InputTxt
+            // 
+            this.InputTxt.Location = new System.Drawing.Point(12, 12);
+            this.InputTxt.Name = "InputTxt";
+            this.InputTxt.Size = new System.Drawing.Size(449, 363);
+            this.InputTxt.TabIndex = 1;
+            this.InputTxt.Text = "";
+            this.InputTxt.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // OutputTxt
+            // 
+            this.OutputTxt.Location = new System.Drawing.Point(468, 13);
+            this.OutputTxt.Name = "OutputTxt";
+            this.OutputTxt.Size = new System.Drawing.Size(320, 362);
+            this.OutputTxt.TabIndex = 2;
+            this.OutputTxt.Text = "";
+            this.OutputTxt.TextChanged += new System.EventHandler(this.RichTextBox2_TextChanged);
+            // 
+            // ClrBtn
+            // 
+            this.ClrBtn.Location = new System.Drawing.Point(598, 400);
+            this.ClrBtn.Name = "ClrBtn";
+            this.ClrBtn.Size = new System.Drawing.Size(92, 23);
+            this.ClrBtn.TabIndex = 3;
+            this.ClrBtn.Text = "Clear\r\n";
+            this.ClrBtn.UseVisualStyleBackColor = true;
+            this.ClrBtn.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClrBtn);
+            this.Controls.Add(this.OutputTxt);
+            this.Controls.Add(this.InputTxt);
+            this.Controls.Add(this.ScanBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ScanBtn;
+        private System.Windows.Forms.RichTextBox InputTxt;
+        private System.Windows.Forms.RichTextBox OutputTxt;
+        private System.Windows.Forms.Button ClrBtn;
+    }
+}
+
---
+++ ScannerProject/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerProject
+{
+    public partial class Form1 : Form
+    {
+        string code;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*states will help in classifying the stuff written
+          which will make it easier in getting tokens */
+
+        //STATES
+        enum states { START, COMMENT, NUMBER, IDENTIFIER, ASSIGN, DONE, RESERVED, STRING, FUNCTION };
+        states state = states.START;
+
+        //RESERVED WORDS
+        string[] ResWords = { "int", "float", "string", "read", "write", "repeat", "until", "if", "elseif", "else", "then", "return", "endl" };
+
+
+        bool isDigit(char d)
+        {
+            if (d >= '0' && d <= '9' || d == '.') return true;
+            else return false;
+        }
+
+        bool isLetter(char l)
+        {
+            if (l >= 'a' && l <= 'z' || l >= 'A' && l <= 'Z') return true;
+            else return false;
+        }
+
+        bool isSymbol(char c)
+        {
+            if ( c == '(' || c == ')' || c == ';' || c == ',' || c == '{' || c == '}') return true;
+            else return false;
+        }
+
+        bool isArithmatic(char A)
+        {
+            if (A == '+' || A == '-' || A == '*' || A == '/') return true;
+            else return false;
+        }
+
+        bool isCondition(char c)
+        {
+            if (c == '=' || c == '<' || c == '>') return true;
+            else return false;
+        }        
+
+        bool isSpace(char s)
+        {
+            if (s == ' ' || s == '\t' || s == '\n') return true;
+            else return false;
+        }
+
+      /*  bool isReserved(string R)
+        {
+            for (int j = 0; j <= ResWords.Length; j++)
+                if (R == ResWords[j])
+                    return true;
+            return false;
+        }*/
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            code = InputTxt.Text;
+            getToken(code += " ");
+        }
+
+        public void getToken(string c)
+        {
+            string token = "";
+            bool ResFlag = false;
+            int i = 0;
+
+            while (state != states.DONE)
+            {
+                switch (state)
+                {
+                    case states.START:
+                        if (isSpace(c[i]))
+                        {
+                            i++;
+                            if (i == c.Length)
+                                state = states.DONE;
+                            else state = states.START;
+                        }
+                        else if (isDigit(c[i]))
+                        {
+                            state = states.NUMBER;
+                        }
+                        else if (isLetter(c[i]))
+                        {
+                            state = states.IDENTIFIER;
+                        }
+                        else if (c[i] == ':')
+                        {
+                            state = states.ASSIGN;
+                        }
+                        else if (c[i] == '^')
+                        {
+                            i++;
+                            state = states.COMMENT;
+                        }
+                        else if (c[i] == '"')
+                        {
+                            i++;
+                            state = states.STRING;
+                        }
+                        else if (isSymbol(c[i]))
+                        {
+                            switch (c[i])
+                            {
+                                case ';':
+                                    OutputTxt.Text += c[i] + "\n";
+                                    break;
+                                default:
+                                    OutputTxt.Text += c[i] + " , symbol \n";
+                                    break;
+                            }
+                            i++;
+                            if (i == c.Length)
+                                state = states.DONE;
+                            else state = states.START;
+                        }
+                        else if(isArithmatic(c[i]))
+                        {
+                            OutputTxt.Text += c[i] + " , arithmatic operator \n";
+                            i++;
+                        }
+                        else if(isCondition(c[i]))
+                        {
+                            OutputTxt.Text += c[i] + " , condition operator \n";
+                            i++;
+                        }
+                        else if(c[i] == '&' && c[i+1] == '&')
+                        {
+                            OutputTxt.Text += "&& , boolean operator \n";
+                            i += 2;
+                        }
+                        else if(c[i]=='|' && c[i+1] =='|')
+                        {
+                            OutputTxt.Text += "|| , boolean operator \n";
+                            i += 2;
+                        }
+                        else if(c[i] == '<' && c[i+1] == '>')
+                        {
+                            OutputTxt.Text += c[i] + c[i + 1] + " , condition operator \n";
+                            i += 2;
+                        }
+                        else state = states.DONE;
+                        break;
+
+                    case states.COMMENT:
+                        if (state == states.COMMENT)
+                        {
+                            while (c[i] != '^')
+                            {
+                                token += c[i];
+                                i++;
+                            }
+                            OutputTxt.Text += token + " , comment \n";
+                            token = "";
+                            i++;
+                            if (i == c.Length) state = states.DONE;
+                            else state = states.START;
+                        }
+                        break;
+
+                    case states.NUMBER:
+                        while (isDigit(c[i]))
+                        {
+                            token += c[i];
+                            i++;
+                        }
+                        OutputTxt.Text += token += " , number \n";
+                        token = "";
+                        if (i == c.Length) state = states.DONE;
+                        else state = states.START;
+                        break;
+
+                    case states.IDENTIFIER:
+                        while (isLetter(c[i]))
+                        {
+                           while(isLetter(c[i]) || isDigit(c[i]))
+                            {
+                                token += c[i];
+                                i++;
+                            }
+                        }
+                        for (int j = 0; j < ResWords.Length; j++)
+                        {
+                            if (ResWords[j] == token) ResFlag = true;
+                        }
+                        if (ResFlag) OutputTxt.Text += token + " , reserved word \n";
+                        else
+                        {
+                            if (c[i] == '(')
+                            {
+                                while (c[i] != ')')
+                                {
+                                    token += c[i];
+                                    i++;
+                                }
+                                i++;
+                                OutputTxt.Text += token + " , function call \n";
+                            }
+                            else OutputTxt.Text += token + " , Identifier \n";
+                        }
+                        token = "";
+                        ResFlag = false;
+                        if (i == c.Length) state = states.DONE;
+                        else state = states.START;
+                        break;
+
+                    /* case states.RESERVED:
+                         while(isLetter(c[i]))
+                         {
+                             token += c[i];
+                             i++;
+                         }
+                         if (isReserved(token))
+                             OutputTxt.Text += token + " , reserved word \n";
+                         token = "";
+                         if (i == c.Length) state = states.DONE;
+                         else state = states.START;
+                         break; */
+
+                    case states.ASSIGN:
+                        if (c[i] == ':')
+                        {
+                            i += 2;
+                            OutputTxt.Text += " := , assign operator \n";
+                            state = states.START;
+                        }
+                        else
+                        {
+                            if (i == c.Length) state = states.DONE;
+                            else state = states.START;
+                        }
+                        break;
+
+                    case states.STRING:
+                        if (state == states.STRING)
+                        {
+                            while (c[i] != '"')
+                            {
+                                token += c[i];
+                                i++;
+                            }
+                            OutputTxt.Text += token + " , string \n";
+                            token = "";
+                            i++;
+                            if (i == c.Length) state = states.DONE;
+                            else state = states.START;
+                        }
+                        break;
+
+                    case states.DONE:
+                        break;
+
+                }
+            }
+        }
+
+
+
+        private void RichTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OutputTxt.Text = "";
+            InputTxt.Text = "";
+            state = states.START;
+        }
+
+
+    }
+}
